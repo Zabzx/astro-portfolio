707b5626966687ab2b095d7d9cc704b15d1f7db1
Ziabeher Phillips <zabq02@gmail.com>
2023-12-08 11:39:52 -0400

    v1

---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				mainBlack: "#1E1E1E",
+				fadedWhite: "rgba(217, 217, 217, 0.5)",
+				fadedBlack: "rgba(0, 0, 0, 0.6)",
+				logoPurple: "#8B33AA",
+				spiceRed: "#AD192A",
+				miniMediaPink: "#FF6884"
+			},
+			backgroundImage: {
+				mainBg: "url(../public/img/bg.jpg)",
+			},
+			fontFamily: {
+				sans: ["Dela Gothic One", ...defaultTheme.fontFamily.sans],
+				"raleway": ["Raleway Variable", ...defaultTheme.fontFamily.sans],
+			},
+			width: {
+				"hero": "500px",
+				"project": "600px",
+			},
+			height: {
+				"hero": "500px",
+				"project": "600px"
+			},
+		},
+	},
+	plugins: [],
+}
